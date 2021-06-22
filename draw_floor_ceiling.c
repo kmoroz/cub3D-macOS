c9c529fad1192afe71124aa0f1ca98dfd2076d88
ksmorozo <ksmorozo@student.codam.nl>
2021-06-22 13:45:21 +0200

    norm edit

--- draw_floor_ceiling.c
+++ draw_floor_ceiling.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/18 16:35:30 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/06/14 21:07:13 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/06/22 13:42:48 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,21 @@ static int	create_rgb(int r, int g, int b)
 
 void	draw_floor(t_settings *settings)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 	int	colour;
 
 	x = 0;
 	y = 0;
-	colour = create_rgb(settings->config->floor_colour[0], settings->config->floor_colour[1], settings->config->floor_colour[2]);
+	colour = create_rgb(settings->config->floor_colour[0],
+			settings->config->floor_colour[1],
+			settings->config->floor_colour[2]);
 	while (y < (settings->config->y_res / 2))
 	{
 		while (x < settings->config->x_res)
 		{
-			my_mlx_pixel_put(settings->window, x, y + settings->config->y_res / 2, colour);
+			my_mlx_pixel_put(settings->window, x,
+				y + settings->config->y_res / 2, colour);
 			x++;
 		}
 		x = 0;
@@ -38,15 +41,17 @@ void	draw_floor(t_settings *settings)
 	}
 }
 
-void draw_ceiling(t_settings *settings)
+void	draw_ceiling(t_settings *settings)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 	int	colour;
 
 	x = 0;
 	y = 0;
-	colour = create_rgb(settings->config->ceiling_colour[0], settings->config->ceiling_colour[1], settings->config->ceiling_colour[2]);
+	colour = create_rgb(settings->config->ceiling_colour[0],
+			settings->config->ceiling_colour[1],
+			settings->config->ceiling_colour[2]);
 	while (y < (settings->config->y_res / 2))
 	{
 		while (x < settings->config->x_res)
