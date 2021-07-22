c88f1c42a6fb88067e00d259f5d337a4ad1e5847
ksmorozo <ksmorozo@student.codam.nl>
2021-07-22 16:07:00 +0200

    edit amount of type identifiers as sprite and resolution are not neccessery anymore

--- checker/check_cub_file.c
+++ checker/check_cub_file.c
@@ -11,14 +11,9 @@ void	count_type_identifiers(t_cub *config, char *line)
 		config->type_identifier_num++;
 	if (ft_strnstr(line, "WE", 2))
 		config->type_identifier_num++;
-	if (ft_strnstr(line, "S ", 2) || (ft_strnstr(line, "S", 1)
-			&& !ft_strnstr(line, "SO", 2)))
-		config->type_identifier_num++;
 	if (ft_strnstr(line, "F", 1))
 		config->type_identifier_num++;
 	if (ft_strnstr(line, "C", 1))
-		config->type_identifier_num++;
-	if (ft_strnstr(line, "R", 1))
 		config->type_identifier_num++;
 }
 
@@ -34,21 +29,20 @@ int	is_type_identifier_allowed(char *line)
 
 void	verify_num_of_type_identifiers(t_cub config)
 {
-	if (config.type_identifier_num < 8)
-		ft_error(FEW_IDENTIFIERS);
-	if (config.type_identifier_num > 8)
-		ft_error(MANY_IDENTIFIERS);
+	if (config.type_identifier_num < TYPE_IDENTIFIERS)
+		ft_error(IDENTIFIERS);
+	if (config.type_identifier_num > TYPE_IDENTIFIERS)
+		ft_error(IDENTIFIERS);
 }
 
 void	check_config_file(t_cub config)
 {
+	verify_num_of_type_identifiers(config);
 	check_player(config);
 	check_map(config);
 	check_max_rgb_value(config);
-	if (config.x_res == 0 || config.y_res == 0)
-		ft_error(ZERO_RES);
+	check_resolution(&config);
 	is_texture_provided(config);
-	verify_num_of_type_identifiers(config);
 	is_rgb_present(config);
 	does_file_exist(config);
 }
